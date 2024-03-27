ef5f39e5235642e48839e20636d82b8cd4cacc92
yash-patel-bacancy <yash.patel@bacancy.com>
2024-03-27 13:49:50 +0530

    Code added for demo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,35 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'data-binding';
+  title = 'Angular training';
+  angularLogo = "assets/angular-old-logo.png";
+  traineeNamesArr = [
+    'Archana Vyas', 
+    'Aryan Prajapati', 
+    'Harmi Ravani',
+    'Jenish Devmurari',
+    'Khushal Rajput',
+    'Krutika Prajapati',
+    'Niksha Patel',
+    'Nilay Doshi',
+    'Parangi Rathod',
+    'Vivek Vaghasiya',
+    'Zenisha Savaliya'
+  ];
+  name = 'Yash';
+
+  userInfo = {
+    name: 'Yash Patel',
+    email: 'yash.patel@bacancy.com',
+    technology: 'Angular',
+    company: 'Bacancy'
+  }
+
+  onUpdateLogo() {
+    this.angularLogo = "assets/angular-logo.png";
+  }
+
+  onAdd() {
+    this.traineeNamesArr.push(this.name);
+  }
 }
